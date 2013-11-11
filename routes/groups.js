350ab5c191571404d16a68c322428db377f09fbb
Fabien Brooke <fsbrookedev@gmail.com>
2013-11-10 21:26:42 -0500

    Show not implemented message for starred and groups pages.

---
+++ routes/groups.js
@@ -0,0 +1,11 @@
+
+module.exports = function(params) {
+  var app = params.app;
+
+  app.get('/groups', getUserGroups);
+};
+
+function getUserGroups(req, res) {
+  if (!req.user) res.redirect('/login');
+  res.end('Not implemented.');
+}
---
+++ routes/starred.js
@@ -0,0 +1,13 @@
+var stories;
+
+module.exports = function(params) {
+  var app = params.app;
+  stories = require('../data/stories.js')(params.db);
+
+  app.get('/starred', getUserStarred);
+};
+
+function getUserStarred(req, res) {
+  if (!req.user) res.redirect('/login');
+  res.end('Not implemented.');
+}
