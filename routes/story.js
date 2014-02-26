5f4bfb775299304c97bed68a810ad686bc44e22c
Fabien Brooke <fabienbrooke@gmail.com>
2014-02-25 23:32:51 -0800

    display whether user has already favorited a story on individual story page

--- routes/story.js
+++ routes/story.js
@@ -14,6 +14,13 @@ function story(req, res, next) {
       return res.render('404');
     }
     if (err) return next(err);
+
+    // provide data for whether a user has starred a story or not if userId is provided
+    if (req.user) {
+      var userId = req.user._id;
+      story.starred = (story.starredBy.indexOf(userId) >= 0);
+    }
+
     res.render('story', { story: story });
   });
 }
