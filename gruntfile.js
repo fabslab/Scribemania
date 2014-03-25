c8168ba953d282dc711df9f903df04cb3855d2e5
Fabien Brooke <fabienbrooke@gmail.com>
2014-03-24 23:40:10 -0700

    add grunt lodash build task

--- gruntfile.js
+++ gruntfile.js
@@ -3,11 +3,29 @@ module.exports = function(grunt) {
   var stylesDirectory = 'public/styles';
 
   grunt.initConfig({
+    exec: {
+      wrap: {
+        command: 'r.js -convert public/scripts public/scripts'
+      }
+    },
     jshint: {
       files: ['gruntfile.js', 'server.js', 'authentication/*.js', 'public/scripts/**/*.js', '!public/scripts/requirejs-config.js',
         'routes/*.js', 'sockets/*.js', 'errors/*.js', 'configuration/*.js'],
       options: {
         jshintrc: '.jshintrc'
+      }
+    },
+    lodash: {
+      build: {
+        dest: 'public/vendor/lodash.js',
+        options: {
+          modifier: 'modern',
+          exports: ['amd', 'commonjs', 'node'],
+          settings: '{ interpolate: /\\{\\{=([\\s\\S]+?)\\}\\}/g, evaluate: /\\{\\{([\\s\\S]+?)\\}\\}/g, escape: /\\{\\{!([\\s\\S]+?)\\}\\}/g }'
+        },
+        flags: [
+          '--minify'
+        ]
       }
     },
     stylus: {
@@ -34,11 +52,6 @@ module.exports = function(grunt) {
         files: [stylesDirectory + '/*.styl'],
         tasks: ['stylus']
       }
-    },
-    exec: {
-      wrap: {
-        command: 'r.js -convert public/scripts public/scripts'
-      }
     }
   });
 
@@ -46,5 +59,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-lodash');
   grunt.registerTask('predeploy', ['stylus', 'exec:wrap']);
 };
