98c84cac0dd4c64a5669cedbdcc213ca17013c38
Fabien Brooke <fabienbrooke@gmail.com>
2014-03-29 20:40:22 -0700

    move ajax prefilters to own module

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,10 @@
 define(function (require, exports, module) {
-var $ = require('jquery');
 
-// set ajax prefilter to send csrf tokens automatically
-var CSRF_HEADER = 'X-CSRF-Token';
-
-var setCSRFToken = function(securityToken) {
-  $.ajaxPrefilter(function(options, _, xhr) {
-    if (!xhr.crossDomain)
-      xhr.setRequestHeader(CSRF_HEADER, securityToken);
-  });
-};
-
-setCSRFToken($('meta[name="csrf-token"]').attr('content'));
-
+require('./common/ajaxPrefilters');
 require('livestamp');
-
 require('./story/main');
 require('./genres/main');
 require('./common/stars');
 require('./common/alerts');
+
 });
