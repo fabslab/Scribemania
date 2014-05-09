68e1e5d3bf96321b1512a93bceaa51ac6a5e370c
Fabien Brooke <fabienbrooke@gmail.com>
2014-05-08 23:17:58 -0700

    load modules dynamically based on loaded page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,10 +5,29 @@ require('./common/stars');
 require('./common/alerts');
 require('livestamp');
 
-require('./start/main');
-require('./summaries/main');
-require('./story/main');
-require('./genres/main');
-require('./groups/main');
+var path = window.location.pathname;
+if (path[path.length - 1] == '/') {
+  path = path.substring(0, path.length - 1);
+}
+
+if (path == '/new') {
+  require(['./new/main']);
+}
+else if (path == '/genres') {
+  require(['./genres/main']);
+}
+else if (path == '/groups') {
+  require(['./groups/main']);
+}
+else if (
+    ['/', '/latest', '/stories', '/starred'].indexOf(path) !== -1 ||
+    path.indexOf('/genres/') === 0 ||
+    path.indexOf('/groups/') === 0
+  ) {
+  require(['./summaries/main']);
+}
+else if (path.indexOf('/stories/') === 0) {
+  require(['./story/main']);
+}
 
 });
