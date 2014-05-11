52080e8234f0ae8c3f02017929bbb8cd08a138db
Fabien Brooke <fabienbrooke@gmail.com>
2014-05-11 00:37:01 -0700

    load genres javascript on individual genre page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,6 +5,7 @@ require('./common/stars');
 require('./common/alerts');
 require('livestamp');
 
+// routing
 var path = window.location.pathname;
 if (path[path.length - 1] == '/') {
   path = path.substring(0, path.length - 1);
@@ -13,17 +14,16 @@ if (path[path.length - 1] == '/') {
 if (path == '/new') {
   require(['./new/main']);
 }
-else if (path == '/genres') {
+else if (path.indexOf('/genres') === 0) {
   require(['./genres/main']);
+  if (path != '/genres') {
+    require(['./summaries/main']);
+  }
 }
 else if (path == '/groups') {
   require(['./groups/main']);
 }
-else if (
-    ['', '/latest', '/stories', '/starred'].indexOf(path) !== -1 ||
-    path.indexOf('/genres/') === 0 ||
-    path.indexOf('/groups/') === 0
-  ) {
+else if (['', '/latest', '/stories', '/starred'].indexOf(path) !== -1 || path.indexOf('/groups/') === 0) {
   require(['./summaries/main']);
 }
 else if (path.indexOf('/stories/') === 0) {
