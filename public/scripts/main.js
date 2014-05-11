b892770d56b238e5d01bd463e7e64770c4f7b1cf
Fabien Brooke <fabienbrooke@gmail.com>
2014-05-10 23:16:09 -0700

    fix routing for homepage

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -20,7 +20,7 @@ else if (path == '/groups') {
   require(['./groups/main']);
 }
 else if (
-    ['/', '/latest', '/stories', '/starred'].indexOf(path) !== -1 ||
+    ['', '/latest', '/stories', '/starred'].indexOf(path) !== -1 ||
     path.indexOf('/genres/') === 0 ||
     path.indexOf('/groups/') === 0
   ) {
