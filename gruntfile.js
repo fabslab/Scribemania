1f9429e0de82e2d9bbf47d42b79cd3579114712a
Fabien Brooke <fabienbrooke@gmail.com>
2014-05-20 00:19:49 -0700

    add requirejs optimization to build process

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,19 @@
-module.exports = function(grunt) {
+var fs = require('fs');
 
-  var stylesDirectory = 'public/styles';
+var stylesDirectory = 'public/styles';
+var requirejsLibs = [];
+
+// this code uses eval which creates a new variable called "require"
+// it is wrapped in a function to avoid overwriting the original node require reference
+(function() {
+  var requirejsConfig = fs.readFileSync('public/scripts/requirejs-config.js');
+  // creates the require variable used in requirejs config to list all library paths
+  requirejsConfig = eval(requirejsConfig + " require");
+  requirejsLibs = Object.keys(requirejsConfig.paths);
+}());
+
+
+module.exports = function(grunt) {
 
   grunt.initConfig({
     exec: {
@@ -29,6 +42,58 @@ module.exports = function(grunt) {
         flags: [
           '--minify'
         ]
+      }
+    },
+    'string-replace': {
+      dist: {
+        files: {
+          'views/layout.jade': 'views/layout.jade'
+        },
+        options: {
+          replacements: [
+            {
+              pattern: /scripts\//gi,
+              replacement: 'scripts-build/'
+            }
+          ]
+        }
+      }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          appDir: 'public/scripts',
+          baseUrl: './',
+          dir: 'public/scripts-build',
+          mainConfigFile: 'public/scripts/requirejs-config.js',
+          optimize: 'uglify2',
+          modules: [
+            {
+              name: 'init/main',
+              exclude: requirejsLibs
+            },
+            {
+              name: 'genres/main',
+              exclude: requirejsLibs
+            },
+            {
+              name: 'groups/main',
+              exclude: requirejsLibs
+            },
+            {
+              name: 'new/main',
+              exclude: requirejsLibs
+            },
+            {
+              name: 'story/main',
+              exclude: requirejsLibs
+            },
+            {
+              name: 'summaries/main',
+              exclude: requirejsLibs
+            }
+          ]
+        }
       }
     },
     stylus: {
@@ -64,8 +129,11 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-lodash');
-  grunt.registerTask('build', ['stylus', 'exec:jade', 'exec:amd']);
+  grunt.loadNpmTasks('grunt-string-replace');
+
+  grunt.registerTask('build', ['exec:jade', 'requirejs', 'string-replace', 'stylus']);
 };
