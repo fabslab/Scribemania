bb2ffda9e7d98e99a79aaf8fe9d808e361fda97a
Fabien Brooke <fabienbrooke@gmail.com>
2014-05-19 22:26:39 -0700

    use relative paths

--- public/scripts/requirejs-config.js
+++ public/scripts/requirejs-config.js
@@ -1,14 +1,14 @@
 var require = {
   // this also gives an overview of all third-party libraries being used
   paths: {
-    favico: '/vendor/favico-0.3.4.min',
-    jquery: '/vendor/jquery.min',
-    jadeRuntime: '/vendor/jade-runtime',
-    livestamp: '/vendor/livestamp',
-    lodash: '/vendor/lodash.min',
-    moment: '/vendor/moment.min',
-    primus: '/vendor/primus',
-    tokeninput: '/vendor/jquery.tokeninput'
+    favico: '../vendor/favico-0.3.4.min',
+    jquery: '../vendor/jquery.min',
+    jadeRuntime: '../vendor/jade-runtime',
+    livestamp: '../vendor/livestamp',
+    lodash: '../vendor/lodash.min',
+    moment: '../vendor/moment.min',
+    primus: '../vendor/primus',
+    tokeninput: '../vendor/jquery.tokeninput'
   },
   shim: {
     'tokeninput': ['jquery']
