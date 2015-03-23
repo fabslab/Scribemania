9b6008c0ab9cd06b476df8e94c5d88454506a06a
Fabien Brooke <fabienbrooke@gmail.com>
2015-03-22 23:44:55 -0700

    update deploy

--- deploy.cmd
+++ deploy.cmd
@@ -95,8 +95,7 @@ call :SelectNodeVersion
 call :ExecuteCmd npm install --loglevel verbose
 
 :: Run grunt
-call :ExecuteCmd npm install grunt-cli -g
-call :ExecuteCmd "./node_modules/.bin/grunt" build --no-color --verbose
+call :ExecuteCmd grunt build --no-color --verbose
 
 :: KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
