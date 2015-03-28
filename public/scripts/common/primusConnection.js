1ba9dcae2d1e64acc904bc59dec9a8a46636bdef
Fabien Brooke <fabienbrooke@gmail.com>
2015-03-27 19:33:32 -0700

    use port 8000 for websockets

--- public/scripts/common/primusConnection.js
+++ public/scripts/common/primusConnection.js
@@ -2,9 +2,15 @@ define(function (require, exports, module) {
 
 var Primus = require('primus');
 
-var url = '';
+// override port to 8000 for openshift server support
+var url = location.origin;
+var port = location.protocol == "http" ? 8000 : 8443;
+if (!url) {
+  url = location.protocol +'//'+ location.hostname;
+}
+url = url.split(':')[0] + port;
 
-var primus = new Primus(url, {
+module.exports = new Primus(url, {
   reconnect: {
     maxDelay: Infinity, // Number: The max delay for a reconnect retry.
     minDelay: 500, // Number: The minimum delay before we reconnect.
@@ -12,7 +18,5 @@ var primus = new Primus(url, {
   },
   strategy: ['disconnect', 'online']
 });
-
-module.exports = primus;
 
 });
