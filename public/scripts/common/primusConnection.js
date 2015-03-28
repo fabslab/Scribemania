8c78470ece6eaab8df5f816df98e664cd5039be8
Fabien Brooke <fabienbrooke@gmail.com>
2015-03-27 22:39:18 -0700

    update url

--- public/scripts/common/primusConnection.js
+++ public/scripts/common/primusConnection.js
@@ -6,10 +6,9 @@ var Primus = require('primus');
 var url = location.origin;
 var port = location.protocol == "http:" ? 8000 : 8443;
 if (!url) {
-  url = location.protocol +'//'+ location.hostname + port;
+  url = location.protocol +'//'+ location.hostname;
 }
-url += location.port || port;
-console.log(url);
+url += ":" + location.port || port;
 
 module.exports = new Primus(url, {
   reconnect: {
