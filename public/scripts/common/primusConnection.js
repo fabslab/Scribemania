3ed7fda6b6331d6bc3f2b398ebeacd702776f67a
Fabien Brooke <fabienbrooke@gmail.com>
2015-03-27 22:22:11 -0700

    update url creation

--- public/scripts/common/primusConnection.js
+++ public/scripts/common/primusConnection.js
@@ -4,11 +4,11 @@ var Primus = require('primus');
 
 // override port to 8000 for openshift server support
 var url = location.origin;
-var port = location.protocol == "http" ? 8000 : 8443;
+var port = location.protocol == "http:" ? 8000 : 8443;
 if (!url) {
-  url = location.protocol +'//'+ location.hostname;
+  url = location.protocol +'//'+ location.hostname + port;
 }
-url = url.split(':')[0] + ':' + port;
+url += location.port || port;
 console.log(url);
 
 module.exports = new Primus(url, {
