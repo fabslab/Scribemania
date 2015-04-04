3bea21fef947689e90ce5f6ea3db9c71e9779e9d
Fabien Brooke <fabienbrooke@gmail.com>
2015-04-03 22:24:25 -0700

    simplify url logic

--- public/scripts/common/primusConnection.js
+++ public/scripts/common/primusConnection.js
@@ -2,13 +2,9 @@ define(function (require, exports, module) {
 
 var Primus = require('primus');
 
-// override port to 8000 for openshift server support
-var url = location.origin;
-var port = location.protocol == "http:" ? 8000 : 8443;
-if (!url) {
-  url = location.protocol +'//'+ location.hostname;
-}
-url += ":" + (location.port || port);
+// default port to 8000 for openshift server websocket support
+var port = location.port || (location.protocol == 'http:' ? 8000 : 8443);
+var url = location.protocol +'//'+ location.hostname + ':' + (location.port || port);
 
 module.exports = new Primus(url, {
   reconnect: {
